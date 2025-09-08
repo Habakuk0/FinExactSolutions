17c5d76e2c13bcf1db09fce8d09624d3cd3cbf97
Habakuk0 <kelvinhabakuk3@gmail.com>
2025-09-08 20:49:21 +0300

    Add company name text next to header logo

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -28,15 +28,16 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <Link href="/" data-testid="link-home-logo">
-                <img
-                  src="/logo.png"
-                  alt="FinExact Solutions Logo"
-                  className="h-16 w-auto cursor-pointer"
-                />
-              </Link>
-            </div>
+            <Link href="/" data-testid="link-home-logo" className="flex items-center space-x-3 cursor-pointer">
+              <img
+                src="/logo.png"
+                alt="FinExact Solutions Logo"
+                className="h-16 w-auto"
+              />
+              <span className="text-2xl font-bold text-primary">
+                FinExact Solutions
+              </span>
+            </Link>
           </div>
           
           {/* Desktop Navigation */}
